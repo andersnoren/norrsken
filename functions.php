8949e5a21b6c96a04cfecfe07a1f6573d5ff0823
Anders <anders@andersnoren.se>
2024-02-04 19:26:25 +0100

    Structured CSS

--- functions.php
+++ functions.php
@@ -29,14 +29,13 @@ if ( ! function_exists( 'norrsken_block_styles' ) ) :
 		/* REGISTER STYLES FOR BLOCKS */
 
 		$core_block_styles = apply_filters( 'norrsken_wp_enqueue_block_style_blocks', array( 
-			'categories', 
-			'comments', 
-			'gallery', 
-			'navigation', 
-			'post-comments-form', 
-			'post-navigation-link', 
+			'paragraph', 
+			'post-comments-form',
+			'post-terms', 
 			'pullquote', 
-			'search' 
+			'quote',
+			'search',
+			'separator'
 		) );
 
 		foreach( $core_block_styles as $block_name ) {
@@ -44,53 +43,118 @@ if ( ! function_exists( 'norrsken_block_styles' ) ) :
 				'core/' . $block_name,
 				array(
 					'handle' => 'norrsken-' . $block_name . '-style',
-					'src'    => get_theme_file_uri( 'assets/css/' . $block_name . '.css' ),
+					'src'    => get_theme_file_uri( 'assets/css/blocks/' . $block_name . '.css' ),
 					'ver'    => wp_get_theme( 'norrsken' )->get( 'Version' ),
 					'path'   => get_theme_file_path( 'assets/css/' . $block_name . '.css' ),
 				)
 			);
 		}
 
+		/* REGISTER STYLES FOR SPECIFIC BLOCK STYLES */
+
+		foreach( array(  'comments-title', 'post-comments-form' ) as $handle ) {
+			wp_enqueue_block_style(
+				'core/' . $handle,
+				array(
+					'handle'	=> 'norrsken-title-separator-right',
+					'src'		=> get_theme_file_uri( 'assets/css/block-styles/norrsken-title-separator-right.css' ),
+					'ver'		=> wp_get_theme( 'norrsken' )->get( 'Version' ),
+					'path'		=> get_theme_file_path( 'assets/css/block-styles/norrsken-title-separator-right.css' ),
+				)
+			);
+		}
+
+		foreach( array(  'post-navigation-link', 'query-pagination' ) as $handle ) {
+			wp_enqueue_block_style(
+				'core/' . $handle,
+				array(
+					'handle'	=> 'norrsken-pagination-post-navigation-link',
+					'src'		=> get_theme_file_uri( 'assets/css/block-styles/norrsken-pagination-post-navigation-link.css' ),
+					'ver'		=> wp_get_theme( 'norrsken' )->get( 'Version' ),
+					'path'		=> get_theme_file_path( 'assets/css/block-styles/norrsken-pagination-post-navigation-link.css' ),
+				)
+			);
+		}
+
+		wp_enqueue_block_style(
+			'core/site-title',
+			array(
+				'handle'	=> 'norrsken-hide-on-home',
+				'src'		=> get_theme_file_uri( 'assets/css/block-styles/norrsken-hide-on-home.css' ),
+				'ver'		=> wp_get_theme( 'norrsken' )->get( 'Version' ),
+				'path'		=> get_theme_file_path( 'assets/css/block-styles/norrsken-hide-on-home.css' ),
+			)
+		);
+
+		wp_enqueue_block_style(
+			'core/post-terms',
+			array(
+				'handle'	=> 'norrsken-post-terms',
+				'src'		=> get_theme_file_uri( 'assets/css/block-styles/norrsken-post-terms.css' ),
+				'ver'		=> wp_get_theme( 'norrsken' )->get( 'Version' ),
+				'path'		=> get_theme_file_path( 'assets/css/block-styles/norrsken-post-terms.css' ),
+			)
+		);
+
 		/* REGISTER BLOCK STYLES */
 
 		register_block_style(
 			'core/comments-title',
 			array(
-				'name'         => 'norrsken-title-separator-right',
-				'label'        => __( 'Separator to the right of the title', 'norrsken' )
+				'is_default'	=> true,
+				'name'			=> 'norrsken-title-separator-right',
+				'label'			=> __( 'Separator to the right of the title', 'norrsken' ),
+				'style_handle'	=> 'norrsken-title-separator-right'
 			)
 		);
 
 		register_block_style(
 			'core/post-comments-form',
 			array(
-				'name'         => 'norrsken-title-separator-right',
-				'label'        => __( 'Separator to the right of the title', 'norrsken' )
+				'is_default'	=> true,
+				'name'			=> 'norrsken-title-separator-right',
+				'label'			=> __( 'Separator to the right of the title', 'norrsken' ),
+				'style_handle'	=> 'norrsken-title-separator-right'
 			)
 		);
 
 		register_block_style(
 			'core/post-navigation-link',
 			array(
-				'name'         => 'norrsken-post-navigation-link',
-				'label'        => __( 'Norrsken', 'norrsken' )
+				'is_default'	=> true,
+				'name'			=> 'norrsken-post-navigation-link',
+				'label'			=> __( 'Norrsken', 'norrsken' ),
+				'style_handle'	=> 'norrsken-pagination-post-navigation-link'
 			)
 		);
 
 		register_block_style(
 			'core/post-terms',
 			array(
-				'name'  => 'norrsken-post-terms',
-				'label' => __( 'Norrsken', 'steve-jang' ),
+				'is_default'	=> true,
+				'name'			=> 'norrsken-post-terms',
+				'label'			=> __( 'Norrsken', 'steve-jang' ),
+				'style_handle'	=> 'norrsken-post-terms'
 			)
 		);
 
 		register_block_style(
 			'core/query-pagination',
 			array(
-				'name'         => 'norrsken-pagination',
-				'label'        => __( 'Norrsken', 'norrsken' ),
-				'inline_style' => "",
+				'is_default'	=> true,
+				'name'			=> 'norrsken-pagination',
+				'label'			=> __( 'Norrsken', 'norrsken' ),
+				'style_handle'	=> 'norrsken-pagination-post-navigation-link'
+			)
+		);
+
+		register_block_style(
+			'core/site-title',
+			array(
+				'is_default'	=> false,
+				'name'			=> 'norrsken-hide-on-home',
+				'label'			=> __( 'Hide on the homepage', 'norrsken' ),
+				'style_handle'	=> 'norrsken-hide-on-home'
 			)
 		);
 		
