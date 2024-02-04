f9e376e76e5a8120b08a254de4a03abce201c454
Anders <anders@andersnoren.se>
2024-02-04 19:28:56 +0100

    Cleaned up patterns, added todo notes to self

--- patterns/hidden-404.php
+++ patterns/hidden-404.php
@@ -6,20 +6,3 @@
  */
 ?>
 
-<!-- wp:heading {"textAlign":"center","level":1,"fontSize":"heading-3"} -->
-<h1 class="wp-block-heading has-text-align-center has-heading-3-font-size" id="page-not-found"><?php echo esc_html_x( 'Error 404', 'Heading for a webpage that is not found', 'norrsken' ); ?></h1>
-<!-- /wp:heading -->
-
-<!-- wp:separator -->
-<hr class="wp-block-separator has-alpha-channel-opacity" />
-<!-- /wp:separator -->
-
-<!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center"><?php echo esc_html_x( 'The page could not be found. Return to the front page or try the search form below.', 'Message to convey that a webpage could not be found', 'norrsken' ); ?></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:group {"layout":{"type":"constrained","contentSize":"280px"}} -->
-<div class="wp-block-group">
-	<!-- wp:pattern {"slug":"norrsken/hidden-search"} /-->
-</div>
-<!-- /wp:group -->
